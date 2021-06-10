d4dd0d044df35d41be1181b60cf210901725f551
Jasper van den Bosch <japsai@gmail.com>
2021-06-10 17:28:59 +0200

    a subjective order (english)

---
+++ analysis/order.py
@@ -0,0 +1,50 @@
+"""A subjective order chosen by the author to roughly reflect commonly
+used object categories (animate, inanimate, tools) followed by the abstract
+words.
+
+A better order for "concreteness" would be one based on behavioral ratings.
+"""
+
+en_order = [
+'face',
+'heart',
+'monkey',
+'tomcat',
+'lamb',
+'falcon',
+'butter',
+'rye',
+'tractor',
+'chair',
+'newspaper',
+'paper',
+'stage',
+'necktie',
+'water',
+'stone',
+'metal',
+'island',
+'treasure',
+
+'column',
+'slogan',
+'vote',
+'peace',
+'theory',
+'criticism',
+'nonsense',
+'need',
+'law',
+'distance',
+'impulse',
+'virtue',
+'picture',
+'sense',
+'pact',
+'value',
+'caution',
+'conclusion',
+'luxury',
+'reform',
+'taboo',
+ ]
